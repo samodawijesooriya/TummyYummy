5a0ae0db7e6f735420fafabeb4d41f4c23d5c63b
samodawijesooriya <samodawijesooriya@gmail.com>
2024-10-02 17:17:21 +0530

    commit 10/2/2024 / 5.17pm

--- app/src/main/java/com/example/test6/HelperClass.java
+++ app/src/main/java/com/example/test6/HelperClass.java
@@ -2,16 +2,30 @@ package com.example.test6;
 
 public class HelperClass {
 
-    String username, email, password;
+    String username;
+    String email;
+    String password;
+
+    String imgUrl;
+
+    public HelperClass(String username, String email, String password, String imgUrl) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
 
     public HelperClass() {
     }
 
-    public HelperClass(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
 
     public String getUsername() {
         return username;
